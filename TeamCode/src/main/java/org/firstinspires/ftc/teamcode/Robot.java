0f7feb0524b909fb38201a6d1c3684aab8e807f7
allisonpowell <allyatar2004@gmail.com>
2022-02-03 17:06:33 -0500

    Alice working on creating encoder goals for slideMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -18,13 +18,15 @@ public class Robot {
     private DcMotorEx frontL;
     private DcMotorEx backR;
     private DcMotorEx backL;
+    private DcMotorEx motorVertSlide;
     private double goalEncoders;
+    private double slideGoal;
 
     private ArrayList<DcMotorEx> motors;
     // frontL, frontR, backL, backR
    // private int encoderValues [] = {0,0,0,0};
 
-    private final int FULL_TURN_ENCODERS = 900;
+    private final int FULL_TURN_ENCODERS = 900; //where is 900 coming from?
 
 //    private DcMotorEx turret;
 //    private DcMotorEx slide;
@@ -47,6 +49,7 @@ public class Robot {
         backR = hw.get(DcMotorEx.class, "backRight");
         frontL = hw.get(DcMotorEx.class, "frontLeft");
         backL = hw.get(DcMotorEx.class, "backLeft");
+        motorVertSlide = hw.get(DcMotorEx.class, "motorVertSlide");
 
 //        turret = hw.get(DcMotorEx.class, "turret");
 //        slide = hw.get(DcMotorEx.class, "slide");
@@ -77,7 +80,7 @@ public class Robot {
         }
     }
 
-    public void setPower(float left, float right){
+    public void setPower(float left, float right){ //what is this doing?
         frontR.setPower(right);
         backR.setPower(right);
         frontL.setPower(left);
@@ -85,29 +88,35 @@ public class Robot {
     }
 
     public void printState(){
+
         t.addData("Encoders Goal: ", goalEncoders);
-        t.addData("FRP: ", frontR.getPower());
-        t.addData("FLP: ", frontL.getPower());
-        t.addData("BRP: ", backR.getPower());
-        t.addData("BLP: ", backL.getPower());
-        t.addData("FRONT RIGHT: ", frontR.getCurrentPosition());
-        t.addData("FRONT LEFT: ", frontL.getCurrentPosition());
-        t.addData("BACK RIGHT: ", backR.getCurrentPosition());
-        t.addData("BACK LEFT", backL.getCurrentPosition());
+        t.addData("FRONT RIGHT POW: ", frontR.getPower());
+        t.addData("FRONT LEFT POW: ", frontL.getPower());
+        t.addData("BACK RIGHT POW: ", backR.getPower());
+        t.addData("BACK LEFT POW:  ", backL.getPower());
+        t.addData("SLIDE POW: ", motorVertSlide.getPower());
+
+        t.addData("FRONT RIGHT POS: ", frontR.getCurrentPosition());
+        t.addData("FRONT LEFT POS: ", frontL.getCurrentPosition());
+        t.addData("BACK RIGHT POS: ", backR.getCurrentPosition());
+        t.addData("BACK LEFT POS:", backL.getCurrentPosition());
+        t.addData("SLIDE POS: ", motorVertSlide.getCurrentPosition());
         t.update();
     }
 
-    private void initMotorList(){
+    private void initMotorList(){ //where is this array called in again?
         motors = new ArrayList<DcMotorEx>();
         motors.add(frontR);
         motors.add(frontL);
         motors.add(backR);
         motors.add(backL);
+        motors.add(motorVertSlide);
     }
 
     public void turn(double degrees, float power){
         double frac = degrees/360.0;
         goalEncoders = frac * FULL_TURN_ENCODERS;
+        //HERE111134ewhfdkfhdsfjhdksjfdjfkdlsjfdklsfjklfjkdsljfdsklfjljfkljflsjfdkljfdlfkj
 
         resetEncoders();
         startEncoders();
